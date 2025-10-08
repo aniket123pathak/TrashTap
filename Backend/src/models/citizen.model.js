ae70fad0f1dc95f98d86bfb49afa2a6174be99d7
Aniket Pathak <aniketpathak581@gmail.com>
2025-10-08 21:44:42 +0530

    Access And Refresh token

--- Backend/src/models/citizen.model.js
+++ Backend/src/models/citizen.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from bcrypt
+import jwt from "jsonwebtoken"
 
 
 const citizenSchema = new Schema ({
@@ -42,5 +43,43 @@ const citizenSchema = new Schema ({
       type: String,
     },
 },{timestamps:true})
+
+citizenSchema.pre("save",async function (next) {
+    if(this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password,10)
+})
+
+citizenSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+citizenSchema.methods.generateAccessToken =  function(){
+    return jwt.sign({
+        _id : this._id,
+        email:this.email,
+        fullName:this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+citizenSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id : this._id,
+        email:this.email,
+        fullName:this.fullName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
 
 export const Citizen = mongoose.model("Citizen",citizenSchema)
--- Backend/src/models/worker.model.js
+++ Backend/src/models/worker.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from bcrypt
+import jwt from "jsonwebtoken";
 
 const workerSchema = new Schema({
     phoneNumber : {
@@ -38,5 +39,42 @@ const workerSchema = new Schema({
       type: String,
     },
 },{timestamps:true})
+
+workerSchema.pre("save",async function (next) {
+    if(this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password,10)
+})
+
+workerSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+workerSchema.methods.generateAccessToken =  function(){
+    return jwt.sign({
+        _id : this._id,
+        email:this.email,
+        fullName:this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+workerSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id : this._id,
+        email:this.email,
+        fullName:this.fullName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const Worker = mongoose.model("Worker",workerSchema)
